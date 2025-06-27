fe4030a8d9ecacad03ef5c769bfa44773c15d993
Suad Omar <suadomar06@gmail.com>
2025-06-27 19:16:43 +0100

    javascript bootcamp code

---
+++ variables.js
@@ -0,0 +1,22 @@
+// var firstName = "Suad" // string
+// var surname = "Omar" // string
+// var age = 19 // number
+// var isMarried = false // boolean
+
+// console.log(age * 2)
+
+// var myCity = 23
+// myCity = "London"
+// console.log(typeof myCity)
+
+// function multiply(num1, num2) {
+//     return num1 * num2
+// }
+
+// console.log(multiply(12,10))
+
+function cube(num){
+    return num * num * num
+}
+
+console.log(cube(4))
